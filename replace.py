7512f957b25697adae8a4594cc3ff25b9d5b4cf0
Ricardo Saldivar <ricardo.rdz@pm.me>
2022-10-15 13:45:39 -0500

    how to use re.sub() function

---
+++ replace.py
@@ -0,0 +1,7 @@
+import re
+
+pattern = '(\d\d\d)-(\d\d\d)-(\d\d\d\d)'
+new_pattern = r'\1\2\3'
+your_input = input('enter: ')
+new_user_input = re.sub(pattern,new_pattern,your_input)
+print(new_user_input)
