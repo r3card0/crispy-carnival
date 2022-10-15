febae91152b76deb4226f844c4b75207b25c2c91
Ricardo Saldivar <ricardo.rdz@pm.me>
2022-10-15 14:06:15 -0500

    learn n,tab

---
+++ tab.py
@@ -0,0 +1,10 @@
+# source: https://www.youtube.com/watch?v=K8L6KVGG-7o
+# Python replaces \t with a tab space
+# Python replaces \n with jump line
+
+
+salto_de_linea = '\nMiles Morales hace salto de linea'
+indentacion = '\tPeter Parker esta indentado'
+
+print(salto_de_linea)
+print(indentacion)
